f63e60d561731c7274536062b8c119e740f96316
rj1337 <mathesonroland@gmail.com>
2025-03-09 17:51:54 +0900

    add test input

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common import by, keys
 
 # Path to Chrome for Testing
 chrome_path = "/opt/google-chrome-test/chrome"
@@ -13,8 +14,10 @@ options.binary_location = chrome_path
 driver = webdriver.Chrome(service=service, options=options)
 
 # Open a webpage
-driver.get("https://www.google.com")
-print(driver.title)
+driver.get("https://ats.rippling.com/playeveryware-careers/jobs/224afeb8-189e-4c29-82ef-27f31f72cf6b/apply?step=application")
+element = driver.find_element(by.By.CSS_SELECTOR, "[data-input='first_name']")
 
-# Close the browser
-driver.quit()
+element.send_keys("Test input")
+element.send_keys(keys.Keys.RETURN)  # Press Enter (if needed)
+
+print(element)
