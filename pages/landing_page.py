f55ee14f508b3030949b8fa265a6634b1d5b832d
rj1337 <mathesonroland@gmail.com>
2025-03-13 19:38:50 +0900

    add wait to logout test

--- pages/landing_page.py
+++ pages/landing_page.py
@@ -6,16 +6,18 @@ from selenium.webdriver.support import expected_conditions as EC
 class LandingPage:
     def __init__(self, driver) -> None:
         self.driver = driver
-        self.menu_button = (By.XPATH, "//*[@id='menu_button_container']/div/div[3]/div/button")
-        self.logout_button = (By.XPATH, "//*[@id='logout_sidebar_link']")
+        self.menu_button = (By.XPATH, "/html/body/div[1]/div[1]/div/div[3]/div/button")
+        self.logout_button = (By.XPATH, "/html/body/div[1]/div[1]/div/div[2]/div[1]/nav/a[3]")
     def open(self, url):
         self.driver.get(url)
     
     def logout(self):
-        WebDriverWait(self.driver, 2).until(
-            EC.element_to_be_clickable(self.menu_button)
-        ).click()
+        self.driver.find_element(*self.menu_button).click()
 
-        WebDriverWait(self.driver, 2).until(
+        WebDriverWait(self.driver, 3).until(
+            EC.visibility_of_element_located(self.logout_button)
+        )
+
+        WebDriverWait(self.driver, 3).until(
             EC.element_to_be_clickable(self.logout_button)
         ).click()
