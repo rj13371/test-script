bd22386cc8cf789fd95df32efc82566077401eae
rj1337 <mathesonroland@gmail.com>
2025-03-19 15:29:30 +0900

    add empty credentials test

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
     def __init__(self, driver) -> None:
@@ -15,3 +17,12 @@ class LoginPage:
         self.driver.find_element(*self.username_field).send_keys(username)
         self.driver.find_element(*self.password_field).send_keys(password)
         self.driver.find_element(*self.submit_button).click()
+    
+    def login_empty_credentials(self, username, password):
+        self.driver.find_element(*self.username_field).send_keys(username)
+        self.driver.find_element(*self.password_field).send_keys(password)
+        self.driver.find_element(*self.submit_button).click()
+        WebDriverWait(self.driver, 1).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "error-button")))
+        
+        self.error_button = self.driver.find_elements(By.CLASS_NAME, "error-button")
